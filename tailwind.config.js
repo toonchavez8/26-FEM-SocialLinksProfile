4cdabde16614a49a3c68823f3c28f33790bdff44
Miguel Chavez <toonchavez8@gmail.com>
2024-01-30 20:22:53 -0600

    Style: add styling for profile componet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"fem-primary-green": "hsl(75, 94, 57)",
+				"fem-White": "hsl(0, 0, 100)",
+				"fem-Grey": "hsl(0, 0, 20)",
+				"fem-Dark-Grey": "hsl(0, 0, 12)",
+				"fem-Off-Black": "hsl(0, 0, 8)",
+			},
+		},
 	},
 	plugins: [],
 };
